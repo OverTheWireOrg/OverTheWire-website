b69b14e0435fc3d435ef21d1e7ee9b22255e1e6b
Steven Van Acker <steven@singularity.be>
2022-01-04 18:38:32 +0100

    update vortex1

--- wargames/vortex/vortex1.c
+++ wargames/vortex/vortex1.c
@@ -1,32 +1,33 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
 
-
-#define e(); if(((unsigned int)ptr & 0xff000000)==0xca000000) { setresuid(geteuid(), geteuid(), geteuid()); execlp("/bin/sh", "sh", "-i", NULL); }
+#define e(); if(((unsigned int)ptr & 0xff000000)==0xca000000) { setresuid(geteuid(), geteuid(), geteuid()); execlp("/bin/sh", "sh", NULL); printf("%p %p\n", &ptr,ptr); }
 
 void print(unsigned char *buf, int len)
 {
-        int i;
+	int i;
 
-        printf("[ ");
-        for(i=0; i < len; i++) printf("%x ", buf[i]); 
-        printf(" ]\n");
+	printf("[ ");
+	for(i=0; i < len; i++) printf("%x ", buf[i]); 
+	printf(" ]\n");
 }
 
 int main()
 {
-        unsigned char buf[512];
-        unsigned char *ptr = buf + (sizeof(buf)/2);
-        unsigned int x;
+	unsigned char buf[512];
+	unsigned char *ptr = buf + (sizeof(buf)/2);
+	unsigned int x;
 
-        while((x = getchar()) != EOF) {
-                switch(x) {
-                        case '\n': print(buf, sizeof(buf)); continue; break;
-                        case '\\': ptr--; break; 
-                        default: e(); if(ptr > buf + sizeof(buf)) continue; ptr++[0] = x; break;
-                }
-        }
-        printf("All done\n");
+	while((x = getchar()) != EOF) {
+		switch(x) {
+			case '\n': print(buf, sizeof(buf)); continue; break;
+			case '\\': ptr--; break; 
+			default: e(); if(ptr > buf + sizeof(buf)) continue; ptr++[0] = x; break;
+		}
+	}
+	printf("All done\n");
+	return 0;
 }
